1645e03280339d67d9f16f8889195f1fd3b6da12
artyom-kalman <p.a.kalashnikov@ya.ru>
2026-09-11 13:05:43 +0900

    Update project structure CONVEX_AGENT_MODE=anonymous bunx convex dev

---
+++ week02/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Artyom";
+
+        System.out.println("Russia");
+        System.out.println("KyungBok University");
+        System.out.printf("Name: %s\n", name);
+    }
+}
