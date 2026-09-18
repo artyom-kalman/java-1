92557bad113a48bd0e0aa7921f0bf61488bb5463
artyom-kalman <p.a.kalashnikov@ya.ru>
2026-09-18 14:14:14 +0900

    Week3

---
+++ week03/Report1.java
@@ -0,0 +1,11 @@
+void main() {
+    boolean flag;
+    byte value1 = -128;
+    short value2 = 23445;
+    int value3 = 534534543;
+    long value4 = 453453463453453l;
+    double value5 = 3.14;
+    float value6 = 4.543f;
+
+    System.out.println(value1);
+}
